9a12ef3cc8c4fab6704ee02d5c1af684c644d51b
frezrik <frezrik@126.com>
2022-01-23 12:34:25 +0800

    添加四大组件getApplicationContext测试

--- app/src/main/java/com/frezrik/jiagu/TestService.java
+++ app/src/main/java/com/frezrik/jiagu/TestService.java
@@ -3,9 +3,16 @@ package com.frezrik.jiagu;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class TestService extends Service {
     public TestService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("JIAGU_TEST", "onCreate[Service] ==> " + getApplicationContext().getClass().getName());
     }
 
     @Override
