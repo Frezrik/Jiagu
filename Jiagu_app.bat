eee9706742d40bbd8bd2849947d0187504e82932
Frezrik <frezrik@126.com>
2022-10-29 00:01:44 +0800

    加固打包时处理raw压缩导致应用崩溃

--- Jiagu_app.bat
+++ Jiagu_app.bat
@@ -4,7 +4,7 @@ echo ===^> app clean...
 call gradlew app:clean >NUL 2>&1
 
 echo ===^> build app...
-call gradlew app:assembleRelease >NUL 2>&1
+call gradlew app:resguardRelease >NUL 2>&1
 
 xcopy app\build\outputs\apk\release\app-release.apk input /y
 
--- Jiagu_app.sh
+++ Jiagu_app.sh
@@ -4,6 +4,6 @@ echo '===> app clean...'
 ./gradlew app:clean >/dev/null 2>&1
 
 echo '===> build app...'
-./gradlew app:assembleRelease >/dev/null 2>&1
+./gradlew app:resguardRelease >/dev/null 2>&1
 
 cp app/build/outputs/apk/release/app-release.apk input/
